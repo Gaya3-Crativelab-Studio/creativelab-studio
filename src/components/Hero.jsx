1e413e7d5c023d541a60f9c213bb399e4cfe5b6d
pavankrishtaiahveguru <pavankrishtaiah@gmail.com>
2026-05-26 18:11:29 +0530

    Updated home page rocket image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
       <img
         src={rocket}
         alt="Creative Decoration"
-        className="hidden lg:block absolute top-1/2 right-[-10%] -translate-y-1/2 w-[48vw] min-w-[1280px] max-w-[1400px] opacity-95 pointer-events-none select-none z-[2] animate-float"
+        className="hidden lg:block absolute top-1/2 right-[-10%] -translate-y-1/2 w-[48vw] min-w-[1100px] max-w-[1300px] opacity-95 pointer-events-none select-none z-[2] animate-float"
       />
 
       {/* Glow Effects */}
